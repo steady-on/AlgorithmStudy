5342cee7e8a0bf2b3aa884d212049b3864dc0405
Roen <whitebsm93@naver.com>
2022-10-21 00:54:04 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.swift
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.swift
@@ -2,12 +2,11 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     var n = n
-    var i = 2
+    var i = 0
     
-    while n > 1 {
+    while n > 0 {
         i += 1
         n /= i
-        
     }
-    return (i-1)
+    return i-1
 }
