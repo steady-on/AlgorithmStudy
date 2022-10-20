6859af476c81eb667e55c00e3c4dfd0d9b7db6b9
Roen <whitebsm93@naver.com>
2022-10-20 23:52:25 +0900

    [level 0] Title: 주사위의 개수, Time: 0.01 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.swift
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.swift
@@ -1,3 +1,3 @@
 import Foundation
 
-func solution(_ box:[Int], _ n:Int) -> Int { box.reduce(1, {$0 * ($1 / n)}) }
+func solution(_ box:[Int], _ n:Int) -> Int { (box[0]/n) * (box[1]/n) * (box[2]/n)}
