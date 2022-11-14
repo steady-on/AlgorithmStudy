076467ef4d7c062c0336575cbd4db55b91912248
Roen <whitebsm93@naver.com>
2022-11-15 01:14:52 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.29 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func solution(_ emergency:[Int]) -> [Int] { 
+    let dictEmergency = Dictionary(uniqueKeysWithValues: zip(emergency.sorted(by: >), 1...emergency.count))
+    return emergency.map { dictEmergency[$0]! }
+}
