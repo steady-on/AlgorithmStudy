02b384dcc0143cf9133180281a3ae45c35debf16
Roen <whitebsm93@naver.com>
2022-11-20 05:11:48 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.swift
@@ -1,10 +1,3 @@
 import Foundation
 
-func solution(_ array:[Int]) -> [Int] {
-    if let m = array.max() {
-        if let i = array.firstIndex(of: m) {
-            return [m, i]
-        }
-    }
-    return [0,0]
-}
+func solution(_ array:[Int]) -> [Int] { [array.max()!, array.firstIndex(of: array.max()!)!] }
