a5ecc1588328cfc3e017d47c1cdd53d65cca6f08
Roen <whitebsm93@naver.com>
2022-11-20 06:09:24 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.11 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862#) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.01 ms
+메모리: 16.3 MB, 시간: 0.11 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.swift
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.swift
@@ -1,17 +1,7 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> Int {
-    var result: Int = -1000000000000000
+    let numbers: [Int] = numbers.sorted()
     
-    for i in 0..<numbers.count {
-        for j in 1..<numbers.count {
-            if (i != j) {
-                if result < numbers[i] * numbers[j] {
-                    result = numbers[i] * numbers[j]
-                }
-            }
-        }
-    }
-    
-    return result
+    return max(numbers[0] * numbers[1], numbers[numbers.count-1] * numbers[numbers.count-2])
 }
