c29409d7a127b94d739c26df842a62834d502c78
Roen <whitebsm93@naver.com>
2022-11-21 11:47:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.14 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.swift
@@ -4,11 +4,7 @@ func solution(_ s:String) -> String {
     var dict:[Character:Int] = [:]
     
     for c in s {
-        if dict.keys.contains(c) {
-            dict[c]! += 1
-        } else {
-            dict[c] = 1
-        }
+        dict[c] = (dict[c] ?? 0) + 1
     }
     
     return String(dict.filter { $1 == 1 }.keys.sorted())
