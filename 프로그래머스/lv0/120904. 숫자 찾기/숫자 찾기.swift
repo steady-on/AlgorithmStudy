8b6e793f89a498579acfea55fe81a432911e60f2
Roen <whitebsm93@naver.com>
2022-11-25 01:36:13 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.swift
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.swift
@@ -3,6 +3,12 @@ import Foundation
 func solution(_ num:Int, _ k:Int) -> Int {
     let num = Array("\(num)")
     let k = String(k).first!
+    
+    if let index = num.firstIndex(of: k) {
+        return index + 1
+    } else {
+        return -1
+    }
 
-    return num.contains(k) ? (num.firstIndex(of: k)! + 1) : -1
+    return  -1
 }
