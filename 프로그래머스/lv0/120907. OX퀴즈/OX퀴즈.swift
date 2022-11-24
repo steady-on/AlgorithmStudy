00e57cc9776e2824da4e0679ff23cc7029658b7f
Roen <whitebsm93@naver.com>
2022-11-25 02:17:34 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func solution(_ quiz:[String]) -> [String] {
+    var quiz:[[String]] = quiz.map { $0.components(separatedBy: " ") }
+    var result: [String] = []
+    
+    for quest in quiz {
+        var temp: String = ""
+        // let (x, y) = (Int(quest[0])!, Int(quest[2])!)
+        
+        if let x = Int(quest[0]), let y = Int(quest[2]) {
+            if quest[1] == "+" {
+                temp = String(x + y)
+            } else {
+                temp = String(x - y)
+            }
+        }
+        
+        temp == quest[4] ? result.append("O") : result.append("X")
+    }
+    
+    return result
+}
