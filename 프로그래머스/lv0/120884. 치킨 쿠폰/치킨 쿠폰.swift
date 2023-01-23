37fdc2906dc44946ef44fac8d8537e4e8724b78f
Roen <whitebsm93@naver.com>
2023-01-23 11:04:18 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.swift
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.swift
@@ -2,19 +2,13 @@ import Foundation
 
 func solution(_ chicken: Int) -> Int {
     var coupon: Int = chicken
-    var remain: Int = 0
     var service: Int = 0
     
-    while coupon > 0 {
-        if remain >= 10 {
-            coupon += remain / 10 * 10
-            remain %= 10
-        }
-        
-        remain += coupon % 10
-        service += coupon / 10
-        coupon = coupon / 10
+    while coupon / 10 != 0 {
+        var temp: Int = coupon / 10
+        service += temp
+        coupon = coupon % 10 + temp
     }
     
-    return service + (remain / 10)
+    return service
 }
