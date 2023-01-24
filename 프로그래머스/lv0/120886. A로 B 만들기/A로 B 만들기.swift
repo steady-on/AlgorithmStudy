934da760c3040d29cf779429993e2653f0e6a108
Roen <whitebsm93@naver.com>
2023-01-25 02:43:19 +0900

    [level 0] Title: A로 B 만들기, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.swift
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-func solution(_ before:String, _ after:String) -> Int {
-    before.sorted() == after.sorted() ? 1 : 0
-    
+func solution(_ before:String, _ after:String) -> Int { 
+    before.sorted() == after.sorted() ? 1 : 0 
 }
