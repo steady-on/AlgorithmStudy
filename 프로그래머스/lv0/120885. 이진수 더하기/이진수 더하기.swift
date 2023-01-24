42e3fe009f3d97f1b5b9eba869c9549c4b479a26
Roen <whitebsm93@naver.com>
2023-01-25 02:41:22 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.swift
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.swift
@@ -1,41 +1,8 @@
 import Foundation
 
 func solution(_ bin1:String, _ bin2:String) -> String {
-    let bin1 = bin1.reversed() + String(repeating: "0", count: 11-bin1.count)
-    let bin2 = bin2.reversed() + String(repeating: "0", count: 11-bin2.count)
+    let bin1 = Int(bin1, radix: 2)!
+    let bin2 = Int(bin2, radix: 2)!
     
-    var result = ""
-    
-    var temp: String = ""
-    for (i, j) in zip(bin1, bin2) {
-        switch (i, j) {
-        case ("0", "0"):
-            result += temp.isEmpty ? "0" : temp
-            temp = ""
-        case ("1", "0"), ("0", "1"):
-            if temp == "1" {
-                result += "0"
-            } else {
-                result += "1"
-                temp = ""
-            }
-        case ("1", "1"):
-            if temp == "1" {
-                result += "1"
-            } else {
-                result += "0"
-                temp = "1"
-            }
-        default:
-            break
-        }
-    }
-        
-    if let index = result.lastIndex(of: "1") {
-        result = String(result[...index])
-    } else {
-        result = "0"
-    }
-    
-    return String(result.reversed())
+    return String(bin1+bin2, radix: 2)
 }
