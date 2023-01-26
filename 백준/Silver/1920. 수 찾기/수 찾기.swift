8eaeae9fab22ba445b15cd92ec8a55d8ff456465
Roen <whitebsm93@naver.com>
2023-01-26 23:49:12 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 89756 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let A = Set(readLine()!.components(separatedBy: " "))
+let _ = readLine()
+let B = readLine()!.components(separatedBy: " ")
+
+for num in B {
+    if A.contains(num) {
+        print(1)
+    } else {
+        print(0)
+    }
+}
