ba4158236e23955f104220c7dfbf4fbbc67006a2
Roen <whitebsm93@naver.com>
2023-01-30 00:49:02 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,4 +1,11 @@
 func solution(_ arr:[Int]) -> [Int] { 
-    let min: Int = arr.min() ?? 1
-    return arr.count == 1 ? [-1] : arr.filter { $0 != min }
+    guard arr.count != 1 else { return [-1] }
+    
+    var arr = arr
+    
+    if let min = arr.min(), let index = arr.firstIndex(of: min) {
+        arr.remove(at: index)
+    }
+    
+    return arr
 }
