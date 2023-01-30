946dffa75006741c340bff39f796d18226a4d9c5
Roen <whitebsm93@naver.com>
2023-01-30 19:14:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,19 +1,7 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var reversed3x: [Int] = []
+    let radix3Reversed = String(n, radix: 3).reversed()
     
-    var n = n
-    while n >= 3 {
-        reversed3x.append(n % 3)
-        n /= 3
-    }
-    reversed3x.append(n)
-    
-    var x10: Int = 0
-    for index in reversed3x.indices {
-        x10 += reversed3x[index] * Int(pow(Double(3), Double(reversed3x.count-index-1)))
-    }
-
-    return x10
+    return Int(String(radix3Reversed), radix:3)!
 }
