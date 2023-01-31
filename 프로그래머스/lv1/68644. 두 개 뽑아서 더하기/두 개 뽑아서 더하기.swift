3166d5bb1b3d15e6a56274c65502ed4f3b26d810
Roen <whitebsm93@naver.com>
2023-02-01 00:59:15 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.54 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.swift
@@ -4,7 +4,7 @@ func solution(_ numbers:[Int]) -> [Int] {
     var result: Set<Int> = []
     
     for row in numbers.indices {
-        for col in numbers.indices {
+        for col in row..<numbers.count {
             if row != col {
                 result.insert( numbers[row] + numbers[col] )
             }
