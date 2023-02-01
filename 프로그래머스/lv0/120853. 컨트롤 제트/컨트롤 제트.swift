733219ce4ef2b891d5ba45dd7481516333db797e
Roen <whitebsm93@naver.com>
2023-02-01 22:26:18 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.swift
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.swift
@@ -8,9 +8,9 @@ func solution(_ s:String) -> Int {
         if i != "Z" {
             result.append(i)
         } else {
-            if !result.isEmpty {
+            // if !result.isEmpty {
                 result.removeLast()
-            }
+            // }
         }
     }
     
