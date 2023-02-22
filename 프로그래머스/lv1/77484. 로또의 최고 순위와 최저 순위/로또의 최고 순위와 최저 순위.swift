81734413c8650fff885625c011b0784660ae2995
Roen <whitebsm93@naver.com>
2023-02-22 15:17:16 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -4,5 +4,5 @@ func solution(_ lottos:[Int], _ winNums:[Int]) -> [Int] {
     let same: Int = winNums.filter { lottos.contains($0) }.count
     let zero: Int = lottos.filter { $0 == 0 }.count
         
-    return [same + zero == 0 ? 6 : 7 - (same + zero), same == 0 ? 6 : 7 - same ]
+    return [min(6, 7 - (same + zero)), min(6, 7 - same)]
 }
