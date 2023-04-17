a5fe114d694a2295087759a70edc0bd368c4d6e3
Roen <roen.steady.on@gmail.com>
2023-04-17 16:17:30 +0900

    [level 1] Title: 모의고사, Time: 5.15 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
-    let one: [Int] = [1, 2, 3, 4, 5]
-    let two: [Int] = [2, 1, 2, 3, 2, 4, 2, 5]
-    let three: [Int] = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    let students:[[Int]] = [one, two, three]
+    let students: [Int:[Int]] = [
+        1 : [1, 2, 3, 4, 5],
+        2 : [2, 1, 2, 3, 2, 4, 2, 5],
+        3 : [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
     
     var results: [Int] = [0, 0, 0]
 
-    for index in answers.indices {
-        for jndex in students.indices {
-            let student = students[jndex]
-            if answers[index] == student[index % student.count] {
-                results[jndex] += 1
+    for (index, value) in answers.enumerated() {
+        for (student, answer) in students {
+            if value == answer[index % answer.count] {
+                results[student - 1] += 1
             }
         }
     }
