25f4d2000c0fdaf064093ab913da535af323b211
Roen <roen.steady.on@gmail.com>
2023-04-18 15:02:39 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.swift
@@ -1,18 +1,13 @@
 import Foundation
 
-func countDivisor(_ num: Int) -> Int {
-    let divisor: [Int] = (1...num).filter { num % $0 == 0 }
-    return divisor.count
-}
-
 func solution(_ left:Int, _ right:Int) -> Int {
     var result: Int = 0
     
     for n in left...right {       
-        if countDivisor(n) % 2 == 0 {
-            result += n
-        } else {
+        if floor(sqrt(Double(n))) == sqrt(Double(n)) {
             result -= n
+        } else {
+            result += n
         }
     }
     
