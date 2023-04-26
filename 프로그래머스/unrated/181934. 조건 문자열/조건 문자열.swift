a64255013402d002cf63abce99ed46712fec12a0
Roen <roen.steady.on@gmail.com>
2023-04-26 14:27:54 +0900

    [unrated] Title: 조건 문자열, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.swift
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.swift
@@ -1,17 +1,11 @@
 import Foundation
 
 func solution(_ ineq:String, _ eq:String, _ n:Int, _ m:Int) -> Int {
-    if eq == "=" {
-        if ineq == ">" {
-            return n >= m ? 1 : 0
-        } else {
-            return n <= m ? 1 : 0
-        }
-    } else {
-        if ineq == ">" {
-            return n > m ? 1 : 0
-        } else {
-            return n < m ? 1 : 0
-        }
+    switch (ineq, eq) {
+        case (">", "="): return n >= m ? 1 : 0
+        case ("<", "="): return n <= m ? 1 : 0
+        case (">", "!"): return n > m ? 1 : 0
+        case ("<", "!"): return n < m ? 1 : 0
+        default: return 0
     }
 }
