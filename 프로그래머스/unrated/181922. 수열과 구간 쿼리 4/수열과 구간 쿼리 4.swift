554893f28abde1ff1ca5804f7880127195b58964
Roen <roen.steady.on@gmail.com>
2023-04-28 13:55:39 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 1.99 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -4,8 +4,9 @@ func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var arr = arr
     
     for query in queries {
-        for i in query[0]...query[1] {
-            if i % query[2] == 0 {
+        let (s, e, k) = (query[0], query[1], query[2])
+        for i in s...e {
+            if i % k == 0 {
                 arr[i] += 1
             }
         }
