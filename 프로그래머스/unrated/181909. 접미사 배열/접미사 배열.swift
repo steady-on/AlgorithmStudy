931940ba6512488f759a6657a1057339da4b8213
Roen <roen.steady.on@gmail.com>
2023-05-17 09:13:27 +0900

    [unrated] Title: 접미사 배열, Time: 0.15 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.swift
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.swift
@@ -4,7 +4,7 @@ func solution(_ myString:String) -> [String] {
     var result = [String]()
     
     for index in myString.indices {
-        let temp = String(myString.suffix(from: index))
+        let temp = String(myString[index...])
         result.append(temp)
     }
     
