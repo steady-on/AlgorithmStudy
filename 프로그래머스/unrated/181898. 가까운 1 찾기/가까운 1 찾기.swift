9bfbc1c9b489eec5b636361bedabd5789fbc8008
Roen <roen.steady.on@gmail.com>
2023-05-18 11:34:09 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.swift
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.swift
@@ -1,7 +1,3 @@
 import Foundation
 
-func solution(_ arr:[Int], _ idx:Int) -> Int {
-    let filt = arr.enumerated().filter { $0.offset >= idx && $0.element == 1 }
-    
-    return filt.isEmpty ? -1 : filt[0].offset
-}
+func solution(_ arr:[Int], _ idx:Int) -> Int { arr[idx...].firstIndex(of: 1) ?? -1 }
