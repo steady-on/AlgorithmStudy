cb269443ea99fcb9a029b1ebe6e26946372df945
Roen <roen.steady.on@gmail.com>
2023-05-20 05:56:23 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.19 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.swift
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.swift
@@ -3,9 +3,7 @@ import Foundation
 func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
     var arr = arr
     
-    queries.forEach {
-        arr.swapAt($0[0], $0[1])
-    }
+    queries.forEach { arr.swapAt($0[0], $0[1]) }
     
     return arr
 }
