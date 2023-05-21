59d14a91578024c579cfa392734501312254ea67
Roen <roen.steady.on@gmail.com>
2023-05-21 14:49:28 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ numList:[Int]) -> Int {
+    let oddSum = stride(from: 0, through: numList.count - 1, by: 2).map { numList[$0] }.reduce(0, +)
+    let evenSum = stride(from: 1, through: numList.count - 1, by: 2).map { numList[$0] }.reduce(0, +)
+    
+    return max(oddSum, evenSum) ?? oddSum
+}
