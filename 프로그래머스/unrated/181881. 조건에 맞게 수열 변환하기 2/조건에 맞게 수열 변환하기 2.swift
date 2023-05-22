da6d02545732f257f7c0d294faa483e3a122d789
Roen <roen.steady.on@gmail.com>
2023-05-22 17:50:44 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.swift
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.swift
@@ -1,23 +1,20 @@
 import Foundation
 
 func solution(_ arr:[Int]) -> Int {
-    var arr = arr
-    var temp = arr
-    var x = 0
+    var before = [Int]()
+    var after = arr
+    var x = -1
     
-    while true {
-        temp = temp.map { x in
+    while before != after {
+        before = after
+        
+        x += 1
+        
+        after = after.map { x in
             if x >= 50, x % 2 == 0 { return x / 2 } 
             else if x < 50, x % 2 == 1 { return x * 2 + 1 }
             else { return x }
-        }
-        
-        if temp == arr {
-            return x
-        } else {
-            x += 1
-            arr = temp
-        }
+        } 
     }
     
     return x
