dc0aa1cc6664cbd42587dc854cd5116c250c5a38
Roen <roen.steady.on@gmail.com>
2023-06-05 01:29:37 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.53 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+struct PersonalityTypeTest {
+    var rScore = 0
+    var tScore = 0
+    var cScore = 0
+    var fScore = 0
+    var jScore = 0
+    var mScore = 0
+    var aScore = 0
+    var nScore = 0
+    
+    var judgeRvsT: Character { rScore >= tScore ? "R" : "T" }
+    var judgeCvsF: Character { cScore >= fScore ? "C" : "F" }
+    var judgeJvsM: Character { jScore >= mScore ? "J" : "M" }
+    var judgeAvsN: Character { aScore >= nScore ? "A" : "N" }
+    
+    lazy var testResult = String([judgeRvsT, judgeCvsF, judgeJvsM, judgeAvsN])
+    
+    mutating func calculateScore(_ question: String, _ choice: Int) {
+        var score: Int {
+            switch choice {
+            case 3, 5: return 1
+            case 2, 6: return 2
+            case 1, 7: return 3
+            default: return 0
+            }
+        }
+        
+        switch question {
+        case "RT":
+            if choice < 4 { rScore += score } else { tScore += score }
+        case "TR":
+            if choice < 4 { tScore += score } else { rScore += score }
+        case "CF":
+            if choice < 4 { cScore += score } else { fScore += score }
+        case "FC":
+            if choice < 4 { fScore += score } else { cScore += score }
+        case "JM":
+            if choice < 4 { jScore += score } else { mScore += score }
+        case "MJ":
+            if choice < 4 { mScore += score } else { jScore += score }
+        case "AN":
+            if choice < 4 { aScore += score } else { nScore += score }
+        default: // "NA"
+            if choice < 4 { nScore += score } else { aScore += score }
+        }
+    }
+}
+
+func solution(_ survey:[String], _ choices:[Int]) -> String {
+    var personalityTypeTest = PersonalityTypeTest()
+    
+    for (question, choice) in zip(survey, choices) {
+        personalityTypeTest.calculateScore(question, choice)
+    }
+    
+    return personalityTypeTest.testResult
+}
