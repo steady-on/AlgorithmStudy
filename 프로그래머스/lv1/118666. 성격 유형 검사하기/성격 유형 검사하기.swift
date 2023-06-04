babee2eb36bd5a4b3e9741c4e291dbbb75abc55e
Roen <roen.steady.on@gmail.com>
2023-06-05 01:50:02 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.99 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.swift
@@ -1,52 +1,30 @@
 import Foundation
 
-struct PersonalityTypeTest {
-    var rScore = 0
-    var tScore = 0
-    var cScore = 0
-    var fScore = 0
-    var jScore = 0
-    var mScore = 0
-    var aScore = 0
-    var nScore = 0
-    
-    var judgeRvsT: Character { rScore >= tScore ? "R" : "T" }
-    var judgeCvsF: Character { cScore >= fScore ? "C" : "F" }
-    var judgeJvsM: Character { jScore >= mScore ? "J" : "M" }
-    var judgeAvsN: Character { aScore >= nScore ? "A" : "N" }
-    
-    lazy var testResult = String([judgeRvsT, judgeCvsF, judgeJvsM, judgeAvsN])
-    
-    mutating func calculateScore(_ question: String, _ choice: Int) {
-        let score = abs(choice - 4)
-        
-        switch question {
-        case "RT":
-            if choice < 4 { rScore += score } else { tScore += score }
-        case "TR":
-            if choice < 4 { tScore += score } else { rScore += score }
-        case "CF":
-            if choice < 4 { cScore += score } else { fScore += score }
-        case "FC":
-            if choice < 4 { fScore += score } else { cScore += score }
-        case "JM":
-            if choice < 4 { jScore += score } else { mScore += score }
-        case "MJ":
-            if choice < 4 { mScore += score } else { jScore += score }
-        case "AN":
-            if choice < 4 { aScore += score } else { nScore += score }
-        default: // "NA"
-            if choice < 4 { nScore += score } else { aScore += score }
-        }
-    }
-}
-
 func solution(_ survey:[String], _ choices:[Int]) -> String {
-    var personalityTypeTest = PersonalityTypeTest()
+    let pairs: [(Character, Character)] = [("R", "T"), ("C", "F"), ("J", "M"), ("A", "N")]
+    var scores: [Character: Int] = ["R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0]
     
     for (question, choice) in zip(survey, choices) {
-        personalityTypeTest.calculateScore(question, choice)
+        let left = question.first!
+        let right = question.last!
+        let score = abs(choice - 4)
+        
+        if choice < 4 {
+            scores[left]! += score 
+        } else {
+            scores[right]! += score
+        }
     }
     
-    return personalityTypeTest.testResult
+    var result = ""
+    
+    for (left, right) in pairs {
+        if scores[left]! >= scores[right]! {
+            result.append(left)
+        } else {
+            result.append(right)
+        }
+    }
+    
+    return result
 }
