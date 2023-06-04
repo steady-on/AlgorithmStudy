09f78302a7e1d528fb56f3fb36a4ac33b7e42647
Roen <roen.steady.on@gmail.com>
2023-06-05 00:14:03 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.swift
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.swift
@@ -11,28 +11,26 @@ func solution(_ n:Int) -> [[Int]] {
     while value <= n * n {
         result[col][row] = value
         value += 1
+        if value > n * n { break }
         
         switch direction % 4 {
-        case 0 where row < n-1 && result[col][row+1] == 0: // right
-            row += 1
-        case 0:
+        case 0: row += 1 // right
+        case 1: col += 1 // down
+        case 2: row -= 1 // left
+        default: col -= 1 // up
+        } 
+        
+        switch direction % 4 {
+        case 0 where row == n-1 || result[col][row+1] != 0:
             direction += 1
-            col += 1
-        case 1 where col < n-1 && result[col+1][row] == 0: // down
-                col += 1
-        case 1:
+        case 1 where col == n-1 || result[col+1][row] != 0:
             direction += 1
-            row -= 1
-        case 2 where row > 0 && result[col][row-1] == 0: // left
-            row -= 1
-        case 2:
+        case 2 where row == 0 || result[col][row-1] != 0:
             direction += 1
-            col -= 1
-        case 3 where col > 0 && result[col-1][row] == 0: // up
-            col -= 1
+        case 3 where col == 0 || result[col-1][row] != 0:
+            direction += 1
         default: 
-            direction += 1
-            row += 1
+            break
         }
     }
     
