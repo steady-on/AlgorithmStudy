f74f7d98318221540da6d4fb7d1029886ecd334c
Roen <roen.steady.on@gmail.com>
2023-06-05 00:23:15 +0900

    [level 0] Title: 옹알이 (1), Time: 0.37 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 func solution(_ babbling:[String]) -> Int {
+    let babbles = ["aya", "ye", "woo", "ma"]
     var result: [String] = []
     
     for babble in babbling {
         var temp = babble
-        for word in ["aya", "ye", "woo", "ma"] {
+        for word in babbles {
             temp = temp.replacingOccurrences(of: word, with: "1")
         }
         result.append(temp)
