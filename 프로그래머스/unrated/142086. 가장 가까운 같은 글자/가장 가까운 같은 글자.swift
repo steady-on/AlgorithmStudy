dda22a157ef8a03e9aa7e54372f84f776db4f43f
Roen <roen.steady.on@gmail.com>
2023-06-06 08:31:10 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 3.76 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    var stack = [Character]()
+    var stack = [Character: Int]()
     var result = [Int]()
     
     for (index, char) in s.enumerated() {
-        if let last = stack.lastIndex(of: char) {
+        if let last = stack[char] { 
             result.append(index - last)
         } else {
             result.append(-1)
         }
         
-        stack.append(char)
+        stack[char] = index
     }
     
     return result
