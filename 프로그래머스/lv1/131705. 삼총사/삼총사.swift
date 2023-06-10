de6363ab5c3e4adefe6be8436ca8b5f424601b5b
Roen <roen.steady.on@gmail.com>
2023-06-10 16:56:18 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.swift
@@ -1,26 +1,15 @@
 import Foundation
 
-func grouped(_ number: [Int]) -> [(Int, Int, Int)] {
+func solution(_ number:[Int]) -> Int {
     let last = number.count
-    var result = [(Int, Int, Int)]()
+    var count = 0
     
     for first in number.indices {
         for second in (first+1) ..< last {
             for third in (second+1) ..< last {
-                result.append((number[first], number[second], number[third]))
+                if number[first] + number[second] + number[third] == 0 { count += 1}
             }
         }
-    }
-    
-    return result
-}
-
-func solution(_ number:[Int]) -> Int {
-    let orderedPair = grouped(number)
-    var count = 0
-    
-    for (first, second, third) in orderedPair where first + second + third == 0 {
-        count += 1
     }
     
     return count
