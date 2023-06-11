f95f452a86558bf6ca07798ab605d35a2379379d
Roen <roen.steady.on@gmail.com>
2023-06-11 16:33:41 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.58 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+func rememberKeymapping(_ keymap: [String]) -> [Character:Int] {
+    var remember = [Character:Int]()
+    
+    for button in keymap {
+        for (num, char) in zip(1..., button) {
+            guard let numberOfPress = remember[char] else {
+                remember[char] = num
+                continue
+            }
+            
+            if numberOfPress > num { remember[char] = num }
+        }
+    }
+    
+    return remember
+}
+
+func solution(_ keymap:[String], _ targets:[String]) -> [Int] {
+    let remember = rememberKeymapping(keymap)
+    var result = [Int]()    
+
+    TargetLoop: for target in targets {
+        var count = 0
+        
+        for char in target {
+            if let numberOfPress = remember[char] {
+                count += numberOfPress
+            } else {
+                result.append(-1)
+                continue TargetLoop
+            }
+        }
+        
+        result.append(count)
+    }
+    
+    return result
+}
