eba4f5e1ffa63a6d1762f8b891e5c19b004422a4
Roen <roen.steady.on@gmail.com>
2023-06-12 02:54:26 +0900

    [level 2] Title: 문자열 압축, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    guard s.count != 1 else { return 1 }
+    
+    var numberWithCount = [Int]()
+    
+    for number in 1..<s.count {
+        var s = s
+        var temp = String()
+        var unit = String()
+        var count = 0
+
+        while s.count >= number {
+            guard unit.isEmpty == false else {
+                unit = String(s.prefix(number))
+                continue
+            }
+            
+            if s.hasPrefix(unit) {
+                count += 1
+                s.removeFirst(number)
+                continue
+            }
+            
+            temp += "\(count > 1 ? "\(count)" : "")\(unit)"
+            unit = ""
+            count = 0
+        }
+        
+        temp += "\(count > 1 ? "\(count)" : "")\(unit)\(s)"
+        numberWithCount.append(temp.count)
+    }
+    
+    let minNumber = numberWithCount.min()!
+    
+    return minNumber < s.count ? minNumber : s.count
+}
