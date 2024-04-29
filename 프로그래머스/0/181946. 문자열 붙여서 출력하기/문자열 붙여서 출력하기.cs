808f7c9a8a7b4782a2bde611f507dcadc44e85b1
Roen <roen.steady.on@gmail.com>
2024-04-29 18:51:09 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 17.95 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        string s1 = input[0];
+        string s2 = input[1];
+        
+        Console.WriteLine($"{s1 + s2}");
+    }
+}
