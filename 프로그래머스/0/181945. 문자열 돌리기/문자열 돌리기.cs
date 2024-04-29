9ca0529f2afac4f9bd5e5e321ff3b12005b3f596
Roen <roen.steady.on@gmail.com>
2024-04-29 18:53:08 +0900

    [level 0] Title: 문자열 돌리기, Time: 21.59 ms, Memory: 16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        string str = Console.ReadLine();
+        
+        foreach (char i in str)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
