600a0ac5170009c4daa20f58ecc85d2668cd582e
Roen <roen.steady.on@gmail.com>
2024-05-03 16:51:26 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.52 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -4,10 +4,7 @@ public class Solution {
     public string solution(string my_string, int k) {
         string answer = "";
         
-        for (int i = 0; i < k; i++)
-        {
-            answer += my_string;
-        }
+        for (int i = 0; i < k; i++) answer += my_string;
         
         return answer;
     }
