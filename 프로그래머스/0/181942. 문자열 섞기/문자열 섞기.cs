5f10ad7b6374bba7f5697255244379d5bb2e9395
Roen <roen.steady.on@gmail.com>
2024-05-03 16:44:30 +0900

    [level 0] Title: 문자열 섞기, Time: 0.36 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.cs
@@ -6,8 +6,7 @@ public class Solution {
         
         for (int i = 0; i < str1.Length; i++)
         {
-            answer += str1[i];
-            answer += str2[i];
+            answer += $"{str1[i]}{str2[i]}";
         }
         
         return answer;
