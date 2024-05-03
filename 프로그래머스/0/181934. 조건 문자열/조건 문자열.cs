33f41c6575a730198fc889f6193660a5407772cb
Roen <roen.steady.on@gmail.com>
2024-05-03 17:35:29 +0900

    [level 0] Title: 조건 문자열, Time: 0.23 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.cs
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cs
@@ -2,28 +2,11 @@ using System;
 
 public class Solution {
     public int solution(string ineq, string eq, int n, int m) {
-        
-        if (ineq.Equals(">"))
-        {
-            if (eq.Equals("="))
-            {
-                return n >= m ? 1 : 0;
-            }
-            else
-            {
-                return n > m ? 1 : 0;
-            }
-        }
-        else
-        {
-            if (eq.Equals("="))
-            {
-                return n <= m ? 1 : 0;
-            }
-            else
-            {
-                return n < m ? 1 : 0;
-            }
-        }
+        string oper = ineq + eq;
+            
+        if (oper == ">=") return n >= m ? 1 : 0;
+        else if (oper == "<=") return n <= m ? 1 : 0;
+        else if (oper == ">!") return n > m ? 1 : 0;                    
+        else return n < m ? 1 : 0;
     }
 }
